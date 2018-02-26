f47e5565879782fc8218f7e1b68282776d819b24
Nakade Hitsujiko <sora8964@gmail.com>
2018-02-26 22:58:00 +0800

    auto commit

--- vj-cfhelper.php
+++ vj-cfhelper.php
@@ -6,16 +6,16 @@ Version: 1.0
 Author: <a href="http://www.vjmedia.com.hk">VJMedia Technical Team</a>
 */
 
-function vjcf_adminmenu(){
-        add_menu_page('Cloudflare Helper', 'CF Helper', 'administrator', __FILE__, 'vjcf_settingspage' , 'dashicons-cloud');
-        add_action( 'admin_init', 'vjcf_pluginsettings' );
-} add_action('admin_menu', 'vjcf_adminmenu');
-
 function vjcf_pluginsettings() {
-        register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_zoneid' );
-        register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_xauth_email' );
-        register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_xauth_key' );
+	register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_zoneid' );
+	register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_xauth_email' );
+	register_setting( 'vjcf-settingsgroup', 'vjmedia_cfhelper_xauth_key' );
 }
+
+function vjcf_adminmenu(){
+	add_menu_page('Cloudflare Helper', 'CF Helper', 'administrator', __FILE__, 'vjcf_settingspage' , 'dashicons-cloud');
+	add_action( 'admin_init', 'vjcf_pluginsettings' );
+} add_action('admin_menu', 'vjcf_adminmenu');
 
 function vjcf_settingspage(){
 ?>
@@ -23,16 +23,14 @@ function vjcf_settingspage(){
 <h1>VJMedia Cloudflare Helper</h1>
 
 <form method="post" action="options.php">
-        <?php settings_fields( 'vjcf-settingsgroup' ); ?>
-        <?php do_settings_sections( 'vjcf-settingsgroup' ); ?>
-        <table class="form-table">
-                <tr valign="top"><th scope="row">Zone ID</th><td><input type="text" name="vjmedia_cfhelper_zoneid" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_zoneid') ); ?>" /></td></tr>
-                <tr valign="top"><th scope="row">XAuth Email</th><td><input type="text" name="vjmedia_cfhelper_xauth_email" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_xauth_email') ); ?>" /></td></tr>
-                <tr valign="top"><th scope="row">XAuth Key</th><td><input type="text" name="vjmedia_cfhelper_xauth_key" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_xauth_key') ); ?>" /></td></tr>
-        </table>
-
-    <?php submit_button(); ?>
-
+	<?php settings_fields( 'vjcf-settingsgroup' ); ?>
+	<?php do_settings_sections( 'vjcf-settingsgroup' ); ?>
+	<table class="form-table">
+		<tr valign="top"><th scope="row">Zone ID</th><td><input type="text" name="vjmedia_cfhelper_zoneid" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_zoneid') ); ?>" /></td></tr>
+		<tr valign="top"><th scope="row">XAuth Email</th><td><input type="text" name="vjmedia_cfhelper_xauth_email" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_xauth_email') ); ?>" /></td></tr>
+		<tr valign="top"><th scope="row">XAuth Key</th><td><input type="text" name="vjmedia_cfhelper_xauth_key" value="<?php echo esc_attr( get_option('vjmedia_cfhelper_xauth_key') ); ?>" /></td></tr>
+	</table>
+	<?php submit_button(); ?>
 </form>
 </div>
 <?php }
@@ -79,18 +77,17 @@ function vjcf_adminbar( $wp_admin_bar ) {
 function vjcf_inlinejs(){ ?>
 <script>
 function vjcf_purge(){
-		var data = {'action': 'vjcf_purge',};
-
-		jQuery.get(ajaxurl, data, function(response) {
-			//console.log(ajaxurl);
-			//console.log(data);
-			response=(JSON.parse(response));
-			if(response.success === true){
-				alert("Success");
-			}else{
-				alert("Fail");
-			}
-		}, "json");
+	var data = {'action': 'vjcf_purge',};
+	jQuery.get(ajaxurl, data, function(response) {
+		//console.log(ajaxurl);
+		//console.log(data);
+		response=(JSON.parse(response));
+		if(response.success === true){
+			alert("Success");
+		}else{
+			alert("Fail");
+		}
+	}, "json");
 }
 </script>
 <?php }add_action( 'admin_print_scripts', 'vjcf_inlinejs' ); 
